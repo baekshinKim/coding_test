98d974d847909f1822ab06554687f45945a6bc3c
baekshinKim <76276050+baekshinKim@users.noreply.github.com>
2022-12-13 14:31:27 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ins.NAME , ins.DATETIME
+from ANIMAL_INS as ins
+left join ANIMAL_OUTS as outs
+on ins.ANIMAL_ID = outs.ANIMAL_ID
+where outs.DATETIME is null
+order by ins.DATETIME asc
+limit 3
