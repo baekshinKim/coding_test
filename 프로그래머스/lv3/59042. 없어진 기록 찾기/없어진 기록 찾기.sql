2dd1810d2a591297a1b518bd6939f22f6bd04641
baekshinKim <76276050+baekshinKim@users.noreply.github.com>
2023-02-01 11:34:25 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -2,7 +2,7 @@
 SELECT o.ANIMAL_ID
       ,o.NAME
   from ANIMAL_OUTS as o
-  left join ANIMAL_INS as i 
+  left outer join ANIMAL_INS as i 
   on i.ANIMAL_ID=o.ANIMAL_ID
   where i.ANIMAL_ID is null
   order by ANIMAL_ID
